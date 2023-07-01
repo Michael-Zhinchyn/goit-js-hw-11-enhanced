45b4206eabae70129e6228c7c51d1508c6f86ea9
Michael-Zhinchyn <124111477+Michael-Zhinchyn@users.noreply.github.com>
2023-07-01 12:15:42 -0500

    js update
    added js logic

--- src/index.js
+++ src/index.js
@@ -1,7 +1,104 @@
-// Підключаємо різні модулі та файлі для роботи з анімацією, lightbox та API
 import './js/animation';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
-
 import { Notify } from 'notiflix';
 import { Report } from 'notiflix/build/notiflix-report-aio';
+import axios from 'axios';
+
+const form = document.getElementById('search-form');
+const gallery = document.querySelector('.gallery');
+const target = document.querySelector('.js-guard');
+
+const BASIC_URL = 'https://pixabay.com/api/';
+const API_KEY = '37718597-f2a776258a6c278a1ed771723';
+
+let searchQuery = '';
+let lightbox;
+let currentPage = 1;
+
+form.addEventListener('submit', onSubmit);
+
+// Функція при натиску кнопки SEARCH
+function onSubmit(evt) {
+  evt.preventDefault();
+  searchQuery = evt.target.elements.searchQuery.value;
+
+  if (searchQuery === '') {
+    Notify.warning('Input field is empty');
+    return;
+  }
+
+  getImages();
+
+  currentPage += 1;
+}
+
+// Функція HTTP запиту і отримання фото
+async function getImages() {
+  const param = new URLSearchParams({
+    key: `${API_KEY}`,
+    q: `${searchQuery}`,
+    image_type: 'photo',
+    orientation: 'horizontal',
+    safesearch: 'true',
+    page: `${currentPage}`,
+  });
+
+  try {
+    const response = await axios.get(`${BASIC_URL}?${param}`);
+    const markUp = createMarkUp(response.data.hits);
+
+    gallery.insertAdjacentHTML('beforeend', markUp);
+    lightbox = new SimpleLightbox('.gallery a', {
+      captionDelay: 200,
+      captionsData: 'alt',
+    });
+
+    const { height: cardHeight } = document
+      .querySelector('.gallery')
+      .firstElementChild.getBoundingClientRect();
+
+    window.scrollBy({
+      top: cardHeight * 2,
+      behavior: 'smooth',
+    });
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+// Функція створення розмітки
+function createMarkUp(data) {
+  return data
+    .map(
+      ({
+        webformatURL,
+        largeImageURL,
+        tags,
+        views,
+        comments,
+        downloads,
+        likes,
+      }) => `
+      <div class="photo-card">
+        <a href="${largeImageURL}">
+          <img class="image" src="${webformatURL}" alt="${tags}" loading="lazy"/>
+          <div class="info">
+            <p class="info-item">
+              <b>Likes: ${likes}</b>
+            </p>
+            <p class="info-item">
+              <b>Views: ${views}</b>
+            </p>
+            <p class="info-item">
+              <b>Comments: ${comments}</b>
+            </p>
+            <p class="info-item">
+              <b>Downloads: ${downloads}</b>
+            </p>
+          </div>
+        </a>
+      </div>`
+    )
+    .join('');
+}
