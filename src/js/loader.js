648148074815d4948ff4163ea6226e8feb273477
Michael-Zhinchyn <124111477+Michael-Zhinchyn@users.noreply.github.com>
2023-07-02 17:19:32 -0500

    Update loader.js

--- src/js/loader.js
+++ src/js/loader.js
@@ -1,4 +1,4 @@
 window.addEventListener('load', function () {
   const loader = document.querySelector('.loader-main');
-  loader.className += ' hidden'; // this adds 'hidden' class to the element
+  loader.className += ' hidden';
 });
