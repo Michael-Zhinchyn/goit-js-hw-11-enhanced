277dbc6282fc4fe2559cf53bb466e76cf0af813b
Michael-Zhinchyn <124111477+Michael-Zhinchyn@users.noreply.github.com>
2023-07-01 22:36:51 -0500

    update

--- src/js/entrance-modal.js
+++ src/js/entrance-modal.js
@@ -4,6 +4,24 @@ const closeModalBtn = document.getElementById('enhanced');
 
 form.style.display = 'none';
 
+let player;
+window.onYouTubeIframeAPIReady = function () {
+  player = new YT.Player('player', {
+    height: '390',
+    width: '640',
+    videoId: '3rmWJAQ0Na4',
+    playerVars: { autoplay: 0, controls: 1, origin: window.location.origin },
+    events: {
+      onReady: onPlayerReady,
+    },
+  });
+};
+
+function onPlayerReady(event) {
+  // Once the player is ready, stop the video (so it does not autoplay)
+  event.target.stopVideo();
+}
+
 closeModalBtn.addEventListener('click', onClick);
 
 function onClick(evt) {
@@ -11,4 +29,5 @@ function onClick(evt) {
 
   form.style.display = 'flex';
   modal.style.display = 'none';
+  player.playVideo();
 }
