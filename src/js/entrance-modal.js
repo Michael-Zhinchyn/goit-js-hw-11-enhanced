468f09c12107af388dcf37a9af95e76d04f46162
Michael-Zhinchyn <124111477+Michael-Zhinchyn@users.noreply.github.com>
2023-07-01 22:51:34 -0500

    update volume

--- src/js/entrance-modal.js
+++ src/js/entrance-modal.js
@@ -1,11 +1,23 @@
 const form = document.querySelector('.search-form');
 const modal = document.querySelector('.modal');
 const closeModalBtn = document.getElementById('enhanced');
+const volumeSlider = document.getElementById('volumeSlider');
+
+volumeSlider.style.display = 'none';
+
+volumeSlider.addEventListener('input', event => {
+  const volume = event.target.value;
+  player.setVolume(volume);
+});
 
 form.style.display = 'none';
 
+document.getElementById('player').style.visibility = 'hidden';
+
 let player;
+
 window.onYouTubeIframeAPIReady = function () {
+  console.log('YouTube API is ready');
   player = new YT.Player('player', {
     height: '390',
     width: '640',
@@ -20,12 +32,14 @@ window.onYouTubeIframeAPIReady = function () {
 function onPlayerReady(event) {
   // Once the player is ready, stop the video (so it does not autoplay)
   event.target.stopVideo();
+  event.target.setVolume(25);
 }
 
 closeModalBtn.addEventListener('click', onClick);
 
 function onClick(evt) {
   evt.preventDefault();
+  volumeSlider.style.display = 'block';
 
   form.style.display = 'flex';
   modal.style.display = 'none';
